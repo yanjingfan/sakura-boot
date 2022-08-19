659605e4d23c56e6ae000fd824b0fac8a0ee0cb2
yangfan <13142186759@163.com>
2022-08-19 10:32:01 +0800

    perf(): 接口日志打印，使用@ApiOperation替换@MyLog

--- sakura-web/src/main/java/com/sakura/common/web/aop/log/LogAspect.java
+++ sakura-web/src/main/java/com/sakura/common/web/aop/log/LogAspect.java
@@ -1,5 +1,6 @@
-package com.sakura.common.aop.log;
+package com.sakura.common.web.aop.log;
 
+import io.swagger.annotations.ApiOperation;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,46 +16,45 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
-import java.util.Date;
 
 /**
- *
  * @description: 自定义日志切面类
- *
  **/
 @Aspect
 @Component
 public class LogAspect {
     private Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
-    @Pointcut("@annotation(com.sakura.common.aop.log.MyLog)")
-    public void myPointCut(){
+    @Pointcut("@annotation(io.swagger.annotations.ApiOperation)")
+    public void myPointCut() {
     }
+
     @Before("myPointCut()")
-    public void doBefore(JoinPoint joinPoint){
+    public void doBefore(JoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         //获取url,请求方法，ip地址，类名以及方法名，参数
-        logger.info("url={},method={},ip={},class_method={},args={}", request.getRequestURI(),request.getMethod(),request.getRemoteAddr(),joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(),joinPoint.getArgs());
+        logger.info("url={},method={},ip={},class_method={},args={}", request.getRequestURI(), request.getMethod(), request.getRemoteAddr(), joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(), joinPoint.getArgs());
     }
+
     @AfterReturning(pointcut = "myPointCut()")
-    public void printLog(JoinPoint joinPoint){
+    public void printLog(JoinPoint joinPoint) {
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletResponse response = attributes.getResponse();
         int status = response.getStatus();
         String statusStr = "";
-        if (status == 200){
+        if (status == 200) {
             statusStr = "成功";
-        }else {
+        } else {
             statusStr = "失败";
         }
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
-        MyLog myLog = method.getAnnotation(MyLog.class);
+        ApiOperation apiOperation = method.getAnnotation(ApiOperation.class);
         String value = null;
-        if (myLog!=null){
-            value = myLog.value();
+        if (apiOperation != null) {
+            value = apiOperation.value();
         }
-        logger.info(new Date()+"-----"+value+"   调用结果："+statusStr);
+        logger.info("调用接口：{}，调用结果：{}", value, statusStr);
     }
 }
