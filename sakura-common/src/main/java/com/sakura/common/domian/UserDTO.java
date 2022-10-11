7915dfc03ee91b17f2c93abc3c35c955c7522aa3
yangfan <13142186759@163.com>
2022-10-11 16:08:05 +0800

    perf(): 公共用户dto字段修改

--- sakura-common/src/main/java/com/sakura/common/domian/UserDTO.java
+++ sakura-common/src/main/java/com/sakura/common/domian/UserDTO.java
@@ -2,6 +2,8 @@ package com.sakura.common.domian;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 /**
@@ -19,17 +21,30 @@ public class UserDTO {
     /**
      * 姓名
      */
+    @NotEmpty
     private String username;
 
     /**
      * 密码
      */
+    @NotEmpty
     private String passwd;
 
     /**
-     * 手机号
+     * 用户头像
      */
-    private String phone;
+    private String icon;
+
+    /**
+     * 邮箱
+     */
+    @Email
+    private String email;
+
+    /**
+     * 用户昵称
+     */
+    private String nickName;
 
     /**
      * 0-普通用户 1-管理员
@@ -39,14 +54,52 @@ public class UserDTO {
     /**
      * 部门ID
      */
-    private String deptId;
+    private String departmentId;
 
     /**
      * 部门名称
      */
-    private String deptName;
+    private String departmentName;
 
-    private List<String> permissionList;
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 第二个手机号
+     */
+    private String mobileTwo;
+
+    /**
+     * 座机号
+     */
+    private String telephone;
+
+    /**
+     * 帐号启用状态：1->启用；0->禁用
+     */
+    private Integer userStatus;
+
+    /**
+     * 用户来源：0->自填；1->管理员添加；2->微信；3：第三方
+     */
+    private Integer source;
+
+    /**
+     * 是否是管理员：1->是；0->否
+     */
+    private Integer admin;
+
+    /**
+     * 排序字段
+     */
+    private Long sort;
+
+    /**
+     * 平台id
+     */
+    private Integer platformId;
 
     /**
      * 菜单
