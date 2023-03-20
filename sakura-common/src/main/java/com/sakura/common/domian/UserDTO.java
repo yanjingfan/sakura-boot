215d31196116a087d6ee122f7b02d807d93d78d3
yangfan <13142186759@163.com>
2023-03-20 16:07:35 +0800

    perf(): satoken中公共用户类字段修改

--- sakura-common/src/main/java/com/sakura/common/domian/UserDTO.java
+++ sakura-common/src/main/java/com/sakura/common/domian/UserDTO.java
@@ -22,59 +22,59 @@ public class UserDTO {
      * 姓名
      */
     @NotBlank(message = "姓名不能为空")
-    private String username;
+    private String lqbUsername;
 
     /**
      * 密码
      */
     @NotBlank(message = "密码不能为空")
-    private String passwd;
+    private String lqbPasswd;
 
     /**
      * 用户头像
      */
-    private String icon;
+    private String lqbIcon;
 
     /**
      * 邮箱
      */
 //    @Valid
 //    @Email
-    private String email;
+    private String lqbEmail;
 
     /**
      * 用户昵称
      */
-    private String nickName;
+    private String lqbNickName;
 
     /**
      * 手机号
      */
-    private String mobile;
+    private String lqbMobile;
 
     /**
      * 第二个手机号
      */
-    private String mobileTwo;
+    private String lqbMobileTwo;
 
     /**
      * 座机号
      */
-    private String telephone;
+    private String lqbTelephone;
 
     /**
      * 用户来源：0->自填；1->管理员添加；2->微信；3：第三方
      */
-    private Integer source;
+    private Integer lqbSource;
 
     /**
      * 排序字段
      */
-    private Long sort;
+    private Long lqbOrderNum;
 
     /**
      * 平台id
      */
-    private Integer platformId;
+    private Integer lqbPlatformId;
 
 }
